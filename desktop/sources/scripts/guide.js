f83bb3e816c86ed50335dba2836d34a84e5ee496
Devine Lu Linvega <aliceffekt@gmail.com>
2018-08-03 14:03:43 +1200

    Trying something

--- desktop/sources/scripts/guide.js
+++ desktop/sources/scripts/guide.js
@@ -8,7 +8,7 @@ function Guide()
   this.el.style.height = "320px";
   this.show_extras = true;
 
-  this.scale = 2; // require('electron').remote.getCurrentWindow().this.scaleFactor;
+  this.scale = require('electron').remote.getCurrentWindow().scaleFactor;
 
   this.start = function()
   {
