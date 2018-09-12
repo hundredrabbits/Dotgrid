b12ea651c97925d0b1f289ddac114bd205d42a56
Devine Lu Linvega <aliceffekt@gmail.com>
2018-09-12 15:33:47 +1200

    Removed devtools

--- desktop/main.js
+++ desktop/main.js
@@ -33,6 +33,7 @@ app.on('ready', () =>
   
   app.win.loadURL(`file://${__dirname}/sources/index.html`);
   app.win.scaleFactor = require('electron').screen.getPrimaryDisplay().scaleFactor;
+  // app.win.toggleDevTools();
   
   app.win.on('closed', () => {
     win = null
