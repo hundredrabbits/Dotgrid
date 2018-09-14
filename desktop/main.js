14c3540f72e7ab8f2c95fe95e31319600b4e4fb8
Devine Lu Linvega <aliceffekt@gmail.com>
2018-09-14 15:13:51 +1200

    Removed console

--- desktop/main.js
+++ desktop/main.js
@@ -22,7 +22,7 @@ app.on('ready', () =>
   })
     
   app.win.loadURL(`file://${__dirname}/sources/index.html`);
-  // app.win.toggleDevTools();
+  app.inspect();
   
   app.win.on('closed', () => {
     win = null
